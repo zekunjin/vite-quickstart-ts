56b0559d05552b15d7529d5551df571cce36ffc1
jinzekun <2804767996@qq.com>
2021-04-25 16:18:57 +0800

    debounce decorator

---
+++ src/decorators/common.ts
@@ -0,0 +1,13 @@
+export const Debounce = (interval: number = 5000) => (
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) => {
+  let handle: number
+  const func = descriptor.value as Function
+  descriptor.value = (...args: any) => {
+    clearTimeout(handle)
+    handle = window.setTimeout(() => func(args), interval)
+  }
+  return descriptor
+}
