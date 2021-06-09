e8a261908df8d7ab6f645ad9249dfa7b080d0420
zekun.jin <zekun.jin@connext.com.cn>
2021-06-09 16:45:19 +0800

    remove multi import

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { FunctionalStore } from '@/utils/provider'
+import user from './modules/user'
 
 export interface IState {
   [state: string]: any
@@ -13,16 +13,16 @@ export interface IModule<S = IState, A = IActions> {
   actions: A
 }
 
-export interface IStore {
-  [key: string]: FunctionalStore<IModule>
-}
+// export interface IStore {
+//   [key: string]: FunctionalStore<IModule>
+// }
 
-const modules: IStore = {}
-const modulesFiles = import.meta.globEager('./modules/*.ts')
+// const modules: IStore = {}
+// const modulesFiles = import.meta.globEager('./modules/*.ts')
 
-Object.keys(modulesFiles).forEach((modulePath) => {
-  const moduleName = modulePath.replace(/^\.\/modules\/(.*)\.\w+$/, '$1')
-  modules[moduleName] = modulesFiles[modulePath].default
-})
+// Object.keys(modulesFiles).forEach((modulePath) => {
+//   const moduleName = modulePath.replace(/^\.\/modules\/(.*)\.\w+$/, '$1')
+//   modules[moduleName] = modulesFiles[modulePath].default
+// })
 
-export default modules
+export default { user }
