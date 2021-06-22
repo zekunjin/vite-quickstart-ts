5008c24277888f01c33e12ba6c4852a87dd998f4
zekun.jin <zekun.jin@connext.com.cn>
2021-06-22 14:14:35 +0800

    add global error/warn handler

--- src/core/monitor/index.ts
+++ src/core/monitor/index.ts
@@ -2,6 +2,12 @@ import { App } from 'vue'
 
 export default {
   install(app: App<Element>) {
-    app.config.errorHandler = undefined
+    app.config.errorHandler = (err) => {
+      console.error(err)
+    }
+
+    app.config.warnHandler = (msg) => {
+      console.warn(msg)
+    }
   }
 }
